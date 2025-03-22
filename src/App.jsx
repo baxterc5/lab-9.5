86ce2499c62c0d9b92304d557ad1ce9187535c10
baxterc5 <131222241+baxterc5@users.noreply.github.com>
2025-03-22 23:55:21 +0000

    names

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Hello from [Your Name]!</h1>
+      <h1>Hey from Chloé!</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           You clicked {count} times
